fa00ffcb98d00a19732599df92fc32085391aadb
Wellsx <46058305+Wellsx@users.noreply.github.com>
2022-12-05 18:23:49 +0100

    updated test results

--- cypress/e2e/open.random.page.cy.js
+++ cypress/e2e/open.random.page.cy.js
@@ -1,5 +1,6 @@
 import HomePage from '../POM/pages/home.page';
 import SideBar from '../POM/components/side.bar';
+import Data from '../support/data';
 
 describe('Open a random page', () => {
   it('Clicks on the Random Page button', () => {
--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,5 +1,6 @@
 import HomePage from '../POM/pages/home.page';
 import SearchBar from '../POM/components/search.bar';
+import Data from '../support/data';
 
 describe('Search for a document', () => {
   it('Click on the search bar and search for a document', () => {
